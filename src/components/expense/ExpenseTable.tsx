81597a9e3432f63e7773027d723397cb409e5d14
Robert Sidzinka <robertsidzinka@interia.pl>
2024-09-28 00:23:54 +0200

    Add default sorting

--- src/components/expense/ExpenseTable.tsx
+++ src/components/expense/ExpenseTable.tsx
@@ -188,6 +188,9 @@ function ExpenseTable({ projectId, expenses }: Props) {
         pagination: {
           paginationModel: { pageSize: 10, page: 0 },
         },
+        sorting: {
+          sortModel: [{ field: "buyDate", sort: "desc" }],
+        },
       }}
     />
   );
