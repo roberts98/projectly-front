f40304142a525f4ee4076438e534ae6195d26409
Robert Sidzinka <robertsidzinka@interia.pl>
2024-10-06 16:06:29 +0200

    Add QR code for displaying user link.

--- src/pages/UserLinksPage.tsx
+++ src/pages/UserLinksPage.tsx
@@ -1,11 +1,16 @@
 import { useUserLinks } from "../hooks/user-link/useUserLinks.hook";
+import QRCode from "react-qr-code";
 
 function UserLinksPage() {
   const { userLink } = useUserLinks();
 
   if (!userLink) return null;
 
-  return <div>{userLink.hash}</div>;
+  return (
+    <QRCode
+      value={`https://roberts98.github.io/projectly-front/#/user-links/process/${userLink.hash}`}
+    />
+  );
 }
 
 export default UserLinksPage;
